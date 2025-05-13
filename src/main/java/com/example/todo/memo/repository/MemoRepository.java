fd7e59393bcfc3ee7c988bd241c06ca3ae14a555
ks12467 <ks12467@naver.com>
2025-05-13 18:29:46 +0900

    [hotfix] repository pageable

--- src/main/java/com/example/todo/memo/repository/MemoRepository.java
+++ src/main/java/com/example/todo/memo/repository/MemoRepository.java
@@ -1,7 +1,10 @@
 package com.example.todo.memo.repository;
 
 import com.example.todo.memo.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    Page<Memo> findAll(Pageable pageable);
 }
